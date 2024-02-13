31e40cb805682282902442fff05957919a8552c5
Pablo Villarino <pavire182000@gmail.com>
2024-02-13 23:53:24 +0100

    versión para recibir datos de QR

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import NotFound from './components/NotFound';
+import NavegadorInteriores from './components/NavegadorInteriores/NavegadorInteriores';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Datos de posición recibidos por QR</h1>
+      <BrowserRouter>
+
+
+
+        <Routes>
+          {/* Rutas cargadas normal (no lazy) */}
+          <Route path='/navigator/:id/:place/:floor' element={<React.Suspense fallback={<p>Cargando...</p>}> <NavegadorInteriores></NavegadorInteriores> </React.Suspense>}></Route>
+
+          {/* Paginas no encontradas */}
+          <Route path="*" element={<React.Suspense fallback={<p>Cargando...</p>}> <NotFound></NotFound> </React.Suspense>}></Route>
+        </Routes>
+
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/NavegadorInteriores/NavegadorInteriores.js
@@ -0,0 +1,20 @@
+import { useParams } from "react-router-dom";
+
+
+const NavegadorInteriores = () => {
+
+    const { id , place , floor} = useParams();
+
+    return (
+      <div className="page">
+        <h1>Home</h1>
+        <p>id: {id}</p>
+        <p>place: {place}</p>
+        <p>floor: {floor}</p>
+        <p>id: {id}</p>
+        <p>id: {id}</p>
+      </div>
+    );
+  }
+  
+  export default NavegadorInteriores;
