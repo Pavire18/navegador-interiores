67f4cf4ee78c6a07e3aabba7a3803fa0af5b0d71
Pablo Villarino <pavire182000@gmail.com>
2024-02-14 23:47:31 +0100

    configuración para ghPages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter, Routes, Route, HashRouter } from "react-router-dom";
+import { Switch, Route, HashRouter } from "react-router-dom";
 import NotFound from './components/NotFound';
 import NavegadorInteriores from './components/NavegadorInteriores/NavegadorInteriores';
 
@@ -10,13 +10,13 @@ function App() {
     <div className="App">
       <h1>Datos de posición recibidos por QR</h1>
       <HashRouter basename='/navegador-interiores'>
-        <Routes>
+        <Switch>
           {/* Rutas cargadas normal (no lazy) */}
           <Route path='/navigator/:id/:place/:floor' element={<React.Suspense fallback={<p>Cargando...</p>}> <NavegadorInteriores></NavegadorInteriores> </React.Suspense>}></Route>
 
           {/* Paginas no encontradas */}
           <Route path="*" element={<React.Suspense fallback={<p>Cargando...</p>}> <NotFound></NotFound> </React.Suspense>}></Route>
-        </Routes>
+        </Switch>
 
       </HashRouter>
     </div>
